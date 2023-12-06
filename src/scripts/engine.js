822d82663e16effcc4418b3107c5dc39ff8e3005
JoaoGabrielSSilva <90883086+JoaoGabrielSSilva@users.noreply.github.com>
2023-12-06 14:15:49 -0300

    implementação parcial da engine

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,5 +1,35 @@
 const state = {
     view:{
-        
-    }
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score")
+    },
+    values: {
+
+    },
+};
+
+function randomSquare(){
+    state.view.squares.forEach((square) => {
+        square.classList.remove("enemy");
+    });
+
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
+
+    randomSquare.classList.add("enemy");
 }
+
+function addListenerHitbox(){
+    state.view.squares.forEach((square) => {
+
+    })
+}
+
+function main(){
+randomSquare()
+}
+
+
+main();
